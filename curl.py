1da1140c4f88a0e84116331b5e9995799579aaab
teja-MXX <teja.mandapakala@gmail.com>
2022-02-24 14:58:41 +0530

    Files Download for Windows/Linux

--- curl.py
+++ curl.py
@@ -6,6 +6,7 @@ import os
 from time import time
 import random
 from colorama import Fore, Style
+import platform
 
 saveFolder = "/home/teja_mxx/Downloads/"
 
@@ -19,6 +20,7 @@ def port_valid(port):
     if(port < 0 and port > 65535):
         print("PortRangeError : Port Address out of range")
 
+# Formatting HTML file to get the files/folders names
 def formatFiles(name):
     name = name[13:-9]
     nameLen = len(name) - 2
@@ -26,6 +28,7 @@ def formatFiles(name):
     name = name[:nameLen]
     return name
 
+# Creating a Temporary Folder to Download all the files in it
 def fileCreate(saveFolder):
     random.seed(int(time()))
     text = ""
@@ -38,22 +41,61 @@ def fileCreate(saveFolder):
     return saveFolder
 
 
-def directoryTraverse(endPoint, banner):
-    resp = requests.get(endPoint, allow_redirects=True)
-    resp = resp.text.split("\n")
-    resp = list(map(unquote, resp[10:-5]))
+def linuX(endPoint, banner):
+    resp = requests.get(endPoint, allow_redirects=True)                       
+    resp = resp.text.split("\n")                 
+    resp = list(map(unquote, resp[10:-5]))                                #URL decoding using unquote
     resp = list(map(formatFiles,resp))
     for dir in resp:
-        
-        if dir[-1] == "/":
-            print(banner + Fore.GREEN + dir)
-            r = directoryTraverse(endPoint+dir,banner[:-4]+"    |___")
+        print(banner + Fore.GREEN + dir)
+    
+        if dir[-1] == "/":                                            
+            dirr = dir
+            if(len(dir.split(" ")) != 1):
+                dirSplit = dir.split(" ")
+                dirr = ""
+                for x in range(0, len(dirSplit)-1 ,1):
+                    dirr += dirSplit[x]+"\ "
+                dirr += dirSplit[-1]
+            
+            endPointSplit = endPoint[len(serverURL):].split(" ")
+            rendPoint= endPoint[len(serverURL):]
+            if(len(endPointSplit) !=1):
+                rendPoint = ""
+                for xy in range(0, len(endPointSplit)-1 ,1):
+                    rendPoint += endPointSplit[xy]+"\ "
+                rendPoint += endPointSplit[-1]
+            
+            os.popen("mkdir "+saveFolder+"/"+rendPoint+dirr)
+            r = linuX(endPoint+dir,banner[:-4]+"    |___")
             
         else:
-            print(banner+dir)
+            data = open(saveFolder+"/"+endPoint[len(serverURL):]+dir, "wb")
+            fileData = requests.get(endPoint+dir, allow_redirects=True)
+            data.write(fileData.content)
+            data.close()
             #print(banner[:-4]+"    |___"+dir)
     return 1
 
+
+def winDows(endPoint, banner):
+    resp = requests.get(endPoint, allow_redirects=True)                       
+    resp = resp.text.split("\n")                 
+    resp = list(map(unquote, resp[10:-5]))                                #URL decoding using unquote
+    resp = list(map(formatFiles,resp))
+
+    
+    for dir in resp:
+        if(dir[-1] == "/"):
+            os.popen("mkdir "+saveFolder+"/"+endPoint[len(serverURL):]+dir)
+            r = winDows(endPoint+dir, banner[:-4]+"    |___")
+        else:
+            data = open(saveFolder+"/"+endPoint[len(serverURL):]+dir, "wb")
+            fileData = requests.get(endPoint+dir, allow_redirects=True)
+            data.write(fileData.content)
+            data.close()
+    
+    return 1
 
 parser = argparse.ArgumentParser()
 sideBanner = "  |___ "
@@ -62,8 +104,14 @@ parser.add_argument("port" , help = "FTP Server Port you want to access", type=p
 args = parser.parse_args()
 ip = argv[1]
 port = argv[2]
-server = "http://"+ip+":"+port+"/"
+serverURL = "http://"+ip+":"+port+"/"
 saveFolder = fileCreate(saveFolder)
 print(saveFolder)
-print("\n ["+server+"]")
-directoryTraverse(server, sideBanner)
+
+if(platform.system() == "Linux"):
+    linuX(serverURL, sideBanner)
+else:
+    winDows(serverURL, sideBanner)
+
+
+#directoryTraverse(serverURL,sideBanner)
