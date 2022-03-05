19c5ca5369feebe94f147ec624b27d4c10c223e4
teja-MXX <teja.mandapakala@gmail.com>
2022-03-05 13:04:34 +0530

    v2.0

--- curl.py
+++ curl.py
@@ -8,7 +8,7 @@ import random
 from colorama import Fore, Style
 import platform
 
-saveFolder = "/home/teja_mxx/Downloads/"
+#dst = "/home/teja_mxx/Downloads/"
 
 def ip_valid(IP):
     ipL = IP.split(".")
@@ -29,16 +29,16 @@ def formatFiles(name):
     return name
 
 # Creating a Temporary Folder to Download all the files in it
-def fileCreate(saveFolder):
+def fileCreate(dst):
     random.seed(int(time()))
     text = ""
     for x in range(5):
         y = random.randrange(65,91)
         text += chr(y)
-    saveFolder += text
-    os.popen("mkdir {}".format(saveFolder))
+    dst += text
+    os.popen("mkdir {}".format(dst))
     print("Temp Directory {} created to save files/folders".format(text))
-    return saveFolder
+    return dst
 
 
 def linuX(endPoint, banner):
@@ -47,34 +47,35 @@ def linuX(endPoint, banner):
     resp = list(map(unquote, resp[10:-5]))                                #URL decoding using unquote
     resp = list(map(formatFiles,resp))
     for dir in resp:
+        
         print(banner + Fore.GREEN + dir)
-    
-        if dir[-1] == "/":                                            
-            dirr = dir
+
+        if dir[-1] == "/":                                    
+            dirr = dir.replace("\'", "\\'")
             if(len(dir.split(" ")) != 1):
-                dirSplit = dir.split(" ")
+                dirSplit = dir.replace("\'", "\\'").split(" ")                                          
                 dirr = ""
                 for x in range(0, len(dirSplit)-1 ,1):
                     dirr += dirSplit[x]+"\ "
                 dirr += dirSplit[-1]
             
-            endPointSplit = endPoint[len(serverURL):].split(" ")
-            rendPoint= endPoint[len(serverURL):]
+            endPointSplit = endPoint[len(serverURL):].replace("\'", "\\'").split(" ")
+            rendPoint= endPoint[len(serverURL):].replace("\'", "\\'")
             if(len(endPointSplit) !=1):
                 rendPoint = ""
                 for xy in range(0, len(endPointSplit)-1 ,1):
                     rendPoint += endPointSplit[xy]+"\ "
                 rendPoint += endPointSplit[-1]
             
-            os.popen("mkdir "+saveFolder+"/"+rendPoint+dirr)
+            os.popen("mkdir "+dst+"/"+rendPoint+dirr)
             r = linuX(endPoint+dir,banner[:-4]+"    |___")
             
         else:
-            data = open(saveFolder+"/"+endPoint[len(serverURL):]+dir, "wb")
+            data = open(dst+"/"+endPoint[len(serverURL):]+dir, "wb")
             fileData = requests.get(endPoint+dir, allow_redirects=True)
             data.write(fileData.content)
             data.close()
-            #print(banner[:-4]+"    |___"+dir)
+            
     return 1
 
 
@@ -87,10 +88,10 @@ def winDows(endPoint, banner):
     
     for dir in resp:
         if(dir[-1] == "/"):
-            os.popen("mkdir "+saveFolder+"/"+endPoint[len(serverURL):]+dir)
+            os.popen("mkdir "+dst+"\\"+endPoint[len(serverURL):]+dir)
             r = winDows(endPoint+dir, banner[:-4]+"    |___")
         else:
-            data = open(saveFolder+"/"+endPoint[len(serverURL):]+dir, "wb")
+            data = open(dst+"/"+endPoint[len(serverURL):]+dir, "wb")
             fileData = requests.get(endPoint+dir, allow_redirects=True)
             data.write(fileData.content)
             data.close()
@@ -101,16 +102,22 @@ parser = argparse.ArgumentParser()
 sideBanner = "  |___ "
 parser.add_argument("ip", help = "IP address of FTP Server", type=ip_valid)
 parser.add_argument("port" , help = "FTP Server Port you want to access", type=port_valid)
+parser.add_argument("dst", help = "Destination URL to save your files")
 args = parser.parse_args()
 ip = argv[1]
 port = argv[2]
+dst = argv[3]+"/"
 serverURL = "http://"+ip+":"+port+"/"
-saveFolder = fileCreate(saveFolder)
-print(saveFolder)
+
+print("Destination Folder - {}".format(dst))
 
 if(platform.system() == "Linux"):
+    dst = argv[3]+"/"
+    dst = fileCreate(dst)
     linuX(serverURL, sideBanner)
 else:
+    dst = argv[3]+"\\"
+    dst = fileCreate(dst)
     winDows(serverURL, sideBanner)
 
 
